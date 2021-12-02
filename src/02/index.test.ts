50136ddb2a28a8d441155f09d6078c605f903f07
Henri Michel <henri@tymate.com>
2021-12-02 06:14:21 +0100

    test: add day 2 tests :snowflake:

--- src/02/index.test.ts
+++ src/02/index.test.ts
@@ -1,11 +1,16 @@
 import { formatInput, partOne, partTwo } from './';
 
-const partOneInput = ``;
+const input = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`;
 
 test('gets correct part one sample answer', () => {
-  // expect(partOne(formatInput(partOneInput))).toBe();
+  expect(partOne(formatInput(input))).toBe(150);
 });
 
 test('gets correct part two sample answer', () => {
-  // expect(partTwo(formatInput(partOneInput))).toBe();
+  expect(partTwo(formatInput(input))).toBe(900);
 });
