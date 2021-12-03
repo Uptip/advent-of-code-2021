955293b23b45200f3f5d0990fe32c0aacc5ef2df
Henri Michel <henri@tymate.com>
2021-12-03 07:35:32 +0100

    feat: complete day 3 :christmas_tree:

--- src/03/index.js
+++ src/03/index.js
@@ -1,9 +1,45 @@
-export const formatInput = input => input;
+export const formatInput = input => input.split('\n');
 
-export const partOne = input => {
-  return input;
-};
+export const partOne = input =>
+  [...Array(input[0].length).keys()].reduce(
+    (acc, curr) => [
+      ...acc,
+      input
+        .map(line => line[curr])
+        .reduce((total, bit) => total + Number(bit), 0),
+    ],
+    [],
+  )
+  |> totalInput => totalInput.map(n => Number(n >= input.length / 2))
+  |> gamma => ({ gamma, epsilon: gamma.map(bit => 1 - bit) })
+  |> ({ gamma, epsilon }) =>
+    parseInt(gamma.join(''), 2) * parseInt(epsilon.join(''), 2);
 
-export const partTwo = input => {
-  return input;
-};
+export const partTwo = input =>
+  [...Array(input[0].length).keys()].reduce(
+    ({ o2Output, co2Output }, index) => ({
+      o2Output: o2Output.filter(
+        line =>
+          Number(line[index]) ===
+          Number(
+            o2Output.reduce((total, line) => total + Number(line[index]), 0) >=
+              o2Output.length / 2,
+          ),
+      ),
+      co2Output: co2Output.filter(line =>
+        co2Output.length === 1
+          ? co2Output
+          : Number(line[index]) ===
+            Number(
+              co2Output.reduce(
+                (total, line) => total + Number(line[index]),
+                0,
+              ) <
+                co2Output.length / 2,
+            ),
+      ),
+    }),
+    { o2Output: input, co2Output: input },
+  )
+  |> ({ o2Output, co2Output }) =>
+    parseInt(o2Output, 2) * parseInt(co2Output, 2);
--- src/03/index.test.js
+++ src/03/index.test.js
@@ -1,13 +1,24 @@
 import { formatInput, partOne, partTwo } from './';
 
-const input = ``;
+const input = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`;
 
 describe('03', () => {
   test('gets correct part one sample answer', () => {
-    // expect(partOne(formatInput(input))).toBe(undefined);
+    expect(partOne(formatInput(input))).toBe(198);
   });
 
   test('gets correct part two sample answer', () => {
-    // expect(partTwo(formatInput(input))).toBe(undefined);
+    expect(partTwo(formatInput(input))).toBe(230);
   });
 });
