0822f1864fa8a826b3aeed3b4d0137e270124a97
Henri Michel <henri@tymate.com>
2021-12-07 06:28:24 +0100

    refactor: refactor day 7 to single-return statement :snowman:

--- src/07/index.js
+++ src/07/index.js
@@ -3,28 +3,23 @@ import { times, max, sum } from 'lodash';
 export const formatInput = input => input.split(',').map(Number);
 
 export const partOne = input =>
-  times(max(input)).reduce((cheapest, position) => {
-    const cost = sum(input.map(crab => Math.abs(crab - position)));
-
-    if (!cheapest || cost < cheapest) {
-      return cost;
-    }
-
-    return cheapest;
-  }, 0);
+  times(max(input)).reduce(
+    (cheapest, position) =>
+      Math.min(cheapest, sum(input.map(crab => Math.abs(crab - position)))),
+    Infinity,
+  );
 
 export const partTwo = input =>
-  times(max(input)).reduce((cheapest, position) => {
-    const cost = sum(
-      input.map(
-        crab =>
-          (Math.abs(crab - position) * (Math.abs(crab - position) + 1)) / 2,
+  times(max(input)).reduce(
+    (cheapest, position) =>
+      Math.min(
+        cheapest,
+        sum(
+          input.map(
+            crab =>
+              (Math.abs(crab - position) * (Math.abs(crab - position) + 1)) / 2,
+          ),
+        ),
       ),
-    );
-
-    if (!cheapest || cost < cheapest) {
-      return cost;
-    }
-
-    return cheapest;
-  }, 0);
+    Infinity,
+  );
