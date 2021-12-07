2b21176e6ff50e15013c5ab346e2561c6fd03ac3
Henri Michel <henri@tymate.com>
2021-12-07 06:24:57 +0100

    refactor: switch forEach loop to reduce :snowman:

--- src/07/index.js
+++ src/07/index.js
@@ -2,33 +2,29 @@ import { times, max, sum } from 'lodash';
 
 export const formatInput = input => input.split(',').map(Number);
 
-export const partOne = input => {
-  let cheapest = null;
-
-  times(max(input)).forEach(position => {
+export const partOne = input =>
+  times(max(input)).reduce((cheapest, position) => {
     const cost = sum(input.map(crab => Math.abs(crab - position)));
+
     if (!cheapest || cost < cheapest) {
-      cheapest = cost;
+      return cost;
     }
-  });
 
-  return cheapest;
-};
+    return cheapest;
+  }, 0);
 
-export const partTwo = input => {
-  let cheapest = null;
-
-  times(max(input)).forEach(position => {
+export const partTwo = input =>
+  times(max(input)).reduce((cheapest, position) => {
     const cost = sum(
       input.map(
         crab =>
           (Math.abs(crab - position) * (Math.abs(crab - position) + 1)) / 2,
       ),
     );
-    if (!cheapest || cost < cheapest) {
-      cheapest = cost;
-    }
-  });
 
-  return cheapest;
-};
+    if (!cheapest || cost < cheapest) {
+      return cost;
+    }
+
+    return cheapest;
+  }, 0);
